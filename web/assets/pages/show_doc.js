b479bb855ef64683e3165cd2eb2b67ca2133225b
Nico Schäfer <n_schaefer11@cs.uni-kl.de>
2021-04-11 12:29:27 +0200

    FMT

--- web/assets/pages/show_doc.js
+++ web/assets/pages/show_doc.js
@@ -32,7 +32,7 @@ function updateLanguage() {
 
 
 function updateDate() {
-  var patch = { "extracted": {"doc_date": parseInt($("#docDateInput").val())} };
+  var patch = { "extracted": { "doc_date": parseInt($("#docDateInput").val()) } };
   $.ajax({
     type: 'PATCH',
     contentType: 'application/json',
@@ -42,8 +42,8 @@ function updateDate() {
 
 function removeExtracted(extracted, type, index) {
   var newMeta = extracted;
-  newMeta.splice(index,1);
-  var patch = { "extracted": {"doc_date": null} };
+  newMeta.splice(index, 1);
+  var patch = { "extracted": { "doc_date": null } };
   patch.extracted[type] = newMeta;
   $.ajax({
     type: 'PATCH',
@@ -73,12 +73,12 @@ function initButtons(docId) {
 
 
   //Title change
-  $("#doctitle").on("keypress", function (e) { if (e.which == 13) {$("#doctitle").blur();  return false; }  });
-  $("#doctitle").on("focusout", function (e) { updateTitle();});
+  $("#doctitle").on("keypress", function (e) { if (e.which == 13) { $("#doctitle").blur(); return false; } });
+  $("#doctitle").on("focusout", function (e) { updateTitle(); });
   //Language change
-  $("#lang").on("keypress", function (e) { if (e.which == 13) {$("#lang").blur();  return false; }  });
-  $("#lang").on("focusout", function (e) { updateLanguage();});
-  
+  $("#lang").on("keypress", function (e) { if (e.which == 13) { $("#lang").blur(); return false; } });
+  $("#lang").on("focusout", function (e) { updateLanguage(); });
+
 }
 
 function createTagButton(docId, tagMap, tagId, clickFunc) {
@@ -86,7 +86,7 @@ function createTagButton(docId, tagMap, tagId, clickFunc) {
   if (!tag) {
     return "";
   }
-  var btn = $("<a href=\"/tags/"+tagId+"\" type=\"button\" class=\"btn btn-secondary btn-sm tag-btn mb-2\" style=\"border-color: transparent;\"></a>");
+  var btn = $("<a href=\"/tags/" + tagId + "\" type=\"button\" class=\"btn btn-secondary btn-sm tag-btn mb-2\" style=\"border-color: transparent;\"></a>");
   btn.text(tag.name);
   btn.css("background-color", tag.color);
   btn.css("color", isDark(tag.color) ? "var(--light)" : "var(--dark)")
@@ -165,7 +165,7 @@ function createTagButtons(docId, tags) {
 $('#datepicker').datepicker();
 $('#datepicker').on('changeDate', function () {
   let val = $('#datepicker').datepicker('getUTCDate').getTime() / 1000;
-  $('#docDateInput').val( val);
+  $('#docDateInput').val(val);
   $('#docDate').text(displayDate(val))
   updateDate();
 });
@@ -181,7 +181,7 @@ function initExtracted(extracted) {
       }
       const row = `<tr><td><a href="${link}">${link}</a><i><span class="text-muted" data-feather=\"x\"></span></i></td></tr>`;
       let elt = $(row)
-      elt.find("i").on("click",removeExtracted.bind(null,extracted.link,"link",i));
+      elt.find("i").on("click", removeExtracted.bind(null, extracted.link, "link", i));
       i++;
       $('#urlTable > tbody:last-child').append(elt);
     });
@@ -194,7 +194,7 @@ function initExtracted(extracted) {
     extracted.email.forEach(function (email) {
       const row = `<tr><td><a href="mailto:${email}">${email}</a><i><span class="text-muted" data-feather=\"x\"></span></i></td></tr>`;
       let elt = $(row)
-      elt.find("i").on("click",removeExtracted.bind(null,extracted.email,"email",i));
+      elt.find("i").on("click", removeExtracted.bind(null, extracted.email, "email", i));
       i++;
       $('#emailsTable > tbody:last-child').append(elt);
     });
@@ -207,7 +207,7 @@ function initExtracted(extracted) {
     extracted.phone.forEach(function (phone) {
       const row = `<tr><td><a href="tel:${phone}">${phone}</a><i><span class="text-muted" data-feather=\"x\"></span></i></td></tr>`;
       let elt = $(row)
-      elt.find("i").on("click",removeExtracted.bind(null,extracted.phone,"phone",i));
+      elt.find("i").on("click", removeExtracted.bind(null, extracted.phone, "phone", i));
       i++;
       $('#phoneTable > tbody:last-child').append(elt);
     });
@@ -220,7 +220,7 @@ function initExtracted(extracted) {
     extracted.iban.forEach(function (iban) {
       const row = `<tr><td>${iban}<i><span class="text-muted" data-feather=\"x\"></span></i></td></tr>`;
       let elt = $(row)
-      elt.find("i").on("click",removeExtracted.bind(null,extracted.iban,"iban",i));
+      elt.find("i").on("click", removeExtracted.bind(null, extracted.iban, "iban", i));
       i++;
       $('#ibanTable > tbody:last-child').append(elt);
     });
